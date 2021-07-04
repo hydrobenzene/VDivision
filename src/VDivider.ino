e015675b645543bfd6d8e6a28c128e6b4082e01b
hydrobenzene <86672580+hydrobenzene@users.noreply.github.com>
2021-07-04 08:58:51 -0600

    Add files via upload

---
+++ src/VDivider.ino
@@ -0,0 +1,16 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  pinMode(A5, INPUT);
+  pinMode(D5, OUTPUT);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  delay(64); // uses a tick time of 64ms
+  int value = 0; // resets value
+  value = map(analogRead(A5), 0, 4095, 0, 255); // maps the light level into a value
+  analogWrite(D5, value); // sets the power to the led to the given value
+}
